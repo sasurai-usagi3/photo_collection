e95c57c9dd1eb87a6cddd15d8da2541337197955
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-27 07:37:45 +0900

    add: webpack設定

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CSSnano = require('cssnano');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    application: './src/javascripts/application.js',
+    style: './src/stylesheets/application.sass'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                new CSSnano({
+                  preset: 'default'
+                })
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'application.css'
+    })
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'docs')
+  }
+}
