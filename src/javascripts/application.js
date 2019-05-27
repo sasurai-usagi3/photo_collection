a6af0917fe7471150a6554bd8b0f1aefa609391d
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-28 08:06:18 +0900

    add: データ読み出し

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,1 +1,11 @@
 import {firebase} from './firebase';
+
+window.addEventListener('load', () => {
+  const database = firebase.database();
+
+  database.ref('albums').once('value').then(snapshot => {
+    const albums = snapshot.val();
+
+    console.log(albums);
+  });
+});
