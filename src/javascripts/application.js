db13ad9956469251ad7841953e9a7e565af37ee2
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-28 08:21:52 +0900

    add: 一覧

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,11 +1,23 @@
 import {firebase} from './firebase';
+import Vue from 'vue';
 
 window.addEventListener('load', () => {
   const database = firebase.database();
+  const listAlbum = new Vue({
+    el: '#js-list-album',
+    data: {
+      albums: []
+    }
+  });
 
   database.ref('albums').once('value').then(snapshot => {
-    const albums = snapshot.val();
+    const rawAlbums = snapshot.val();
+    let albums = [];
 
-    console.log(albums);
+    for(let key in rawAlbums) {
+      albums.push(rawAlbums[key]);
+    }
+
+    listAlbum.albums = albums;
   });
 });
