9cd5e7dbaa8ec8f229f42a6808aaf8fe1ec80909
宇山 拓夢 <sasurai.usagi3@gmail.com>
2019-05-28 21:23:00 +0900

    add: データ読み出し

--- src/javascripts/show.js
+++ src/javascripts/show.js
@@ -3,4 +3,25 @@ import Vue from 'vue';
 
 window.addEventListener('load', () => {
   const database = firebase.database();
+  const queryAlbumId = location.search
+    .slice(1)
+    .split('&')
+    .map(x => x.split('='))
+    .find(x => x[0] == 'album_id');
+
+  if (queryAlbumId == null) {
+    return;
+  }
+
+  const albumId = queryAlbumId[1] || '';
+
+  database.ref(`/contents/albums/${albumId}`).once('value').then(snapshot => {
+    const photographs = snapshot.val();
+
+    if (photographs == null) {
+      return;
+    }
+
+    console.log(photographs);
+  });
 });
